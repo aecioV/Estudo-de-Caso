9e3eae74acc4435ed9c62779753df54d219554ba
aecioV <129795938+aecioV@users.noreply.github.com>
2023-04-27 20:34:23 -0300

    Add files via upload

---
+++ EstudoDeCaso-main/Estudo de caso - POO/Estudo de Caso/src/CaixaPOO/TesteCaixa/Main.java
@@ -0,0 +1,32 @@
+package CaixaPOO.TesteCaixa;
+import CaixaPOO.CadastroContas;
+import CaixaPOO.Cliente;
+import CaixaPOO.Terminal;
+import CaixaPOO.Conta;
+
+
+public class Main {
+    public static void main(String[] args){
+
+  
+        Cliente a1 = new Cliente("001", "Aecio");
+        Cliente b2 = new Cliente("002", "Marcos");
+        Cliente c3 = new Cliente("003", "Osvaldo");
+        Cliente d4 = new Cliente("004", "Evaldinolia");
+        Conta A1 = new Conta(312, a1, 433, 900.0);
+        Conta B2 = new Conta(761, b2, 757, 700.0);
+        Conta C3 = new Conta(331, c3, 747, 1800.0);
+        Conta D4 = new Conta(105, d4, 737, 2100.0);
+        CadastroContas cadastro = new CadastroContas(4);
+              
+          cadastro.adicionaConta(A1);
+          cadastro.adicionaConta(B2);
+          cadastro.adicionaConta(C3);
+          cadastro.adicionaConta(D4);
+  
+          Terminal bb = new Terminal(cadastro);
+        
+          bb.iniciaOperacao();
+        
+    }
+}
